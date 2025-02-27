1a70625c9d8430f4b99c460bcbe4cda2ca51736c
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:29:10 +0400

    Done Search apperance  Part

--- src/Component/Header/Search.tsx
+++ src/Component/Header/Search.tsx
@@ -1,3 +1,19 @@
 export default function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="md:block hidden">
+      <div className="md:block hidden">
+        <div className="flex items-center w-[240px] h-[45px] border border-gray-300 rounded-[40px] px-3 py-2">
+          <input
+            type="text"
+            placeholder="Որոնում"
+            className="flex-grow outline-none text-[#dcdddf] bg-inherit "
+          />
+          <img
+            src="https://amaranoc.am/images/header/search.svg"
+            className="w-[16px] h-[16px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
