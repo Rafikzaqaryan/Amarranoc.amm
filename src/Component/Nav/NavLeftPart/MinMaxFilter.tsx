7cee2b86d23911d191a1e262865d5340a53b8daa
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 01:15:35 +0400

    Done MinMaxFIlter Functionality Part

--- src/Component/Nav/NavLeftPart/MinMaxFilter.tsx
+++ src/Component/Nav/NavLeftPart/MinMaxFilter.tsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
+import { useFilter } from "../../../FilterContext";
 
 export default function MinMaxFilter() {
   const [activeButton, setActiveButton] = useState<number | null>(null);
   const buttonValue = ["֏", "$", "€", "₽"];
+  const { setMinPrice, setMaxPrice, minPrice, maxPrice } = useFilter();
+  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleClick = (index: number) => {
+    setActiveButton(index);
+  };
   return (
     <div>
       <div className="flex gap-2 pl-2 pt-[40px]">
@@ -10,29 +25,36 @@ export default function MinMaxFilter() {
         {buttonValue.map((value, index) => (
           <button
             key={index}
+            onClick={() => handleClick(index)}
             className={`w-[34px] h-[34px] border flex justify-center items-center rounded-[20px] text-[18px] cursor-pointer border-[#d8d9dc] 
-      ${
-        activeButton === index ? "bg-black text-white" : "bg-white text-black"
-      }`}
+          ${
+            activeButton === index
+              ? "bg-black text-white"
+              : "bg-white text-black"
+          }`}
           >
             {value}
           </button>
         ))}
-        <div className="flex gap-2 items-center pt-[20px]">
-          <input
-            type="number"
-            placeholder="Սկսած"
-            className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
-            min="0"
-          />
-          -
-          <input
-            type="number"
-            placeholder="Մինչև"
-            className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
-            min="0"
-          />
-        </div>
+      </div>
+      <div className="flex gap-2 items-center pt-[20px]">
+        <input
+          type="number"
+          value={minPrice ?? ""}
+          placeholder="Սկսած"
+          onChange={handleMinPriceChange}
+          className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
+          min="0"
+        />
+        -
+        <input
+          type="number"
+          value={maxPrice ?? ""}
+          placeholder="Մինչև"
+          onChange={handleMaxPriceChange}
+          className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
+          min="0"
+        />
       </div>
     </div>
   );
