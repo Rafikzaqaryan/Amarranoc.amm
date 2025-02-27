1a36a899f6c8437815cf0e695b335cbf5630e0a3
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:39:51 +0400

    Starting do Nav apperance part

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./Component/Header/Header";
+import Nav from "./Component/Nav/Nav";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Nav />
     </div>
   );
 }
