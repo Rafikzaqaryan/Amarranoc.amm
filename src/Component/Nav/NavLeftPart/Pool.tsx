faaa1efeec81777863015cf610cb5225e500b7c7
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 01:21:27 +0400

    Done Pool Functionality Part

--- src/Component/Nav/NavLeftPart/Pool.tsx
+++ src/Component/Nav/NavLeftPart/Pool.tsx
@@ -1,3 +1,61 @@
+import { useState } from "react";
+import { useFilter } from "../../../FilterContext";
+
 export default function Pool() {
-  return <div>Pool</div>;
+  const { setPoolFilters } = useFilter();
+  const [activeButton, setActiveButton] = useState<string | null>(null);
+
+  const handlePoolFilter = (value: string | "all") => {
+    if (value === "all") {
+      setPoolFilters("");
+    } else {
+      setPoolFilters(value);
+    }
+    setActiveButton(value);
+  };
+
+  const buttonClasses = (value: string) =>
+    `h-[50px] border rounded-[40px] border-[#d8d9dc] cursor-pointer ${
+      activeButton === value
+        ? "bg-black text-white"
+        : "bg-white text-black hover:bg-[#eeee]"
+    }`;
+
+  return (
+    <div className="pt-[30px]">
+      <h1 className="text-[black]">Լողավազան</h1>
+      <div className="flex flex-wrap gap-x-4 gap-y-1 pt-[10px]">
+        <button
+          onClick={() => handlePoolFilter("all")}
+          className={buttonClasses("all") + " px-6"}
+        >
+          Բոլորը
+        </button>
+        <button
+          onClick={() => handlePoolFilter("Բաց")}
+          className={buttonClasses("Բաց") + " px-5"}
+        >
+          Բաց
+        </button>
+        <button
+          onClick={() => handlePoolFilter("Փակ")}
+          className={buttonClasses("Փակ") + " px-5"}
+        >
+          Փակ
+        </button>
+        <button
+          onClick={() => handlePoolFilter("Տաքացվող")}
+          className={buttonClasses("Տաքացվող") + " px-6"}
+        >
+          Տաքացվող
+        </button>
+        <button
+          onClick={() => handlePoolFilter("Առանց լողավազանի")}
+          className={buttonClasses("Առանց լողավազանի") + " px-4"}
+        >
+          Առանց լողավազանի
+        </button>
+      </div>
+    </div>
+  );
 }
