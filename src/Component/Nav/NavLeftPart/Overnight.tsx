73089a5f45d561f343734d212b9f11466ef07cf1
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:52:03 +0400

    Done Overnight Part

--- src/Component/Nav/NavLeftPart/Overnight.tsx
+++ src/Component/Nav/NavLeftPart/Overnight.tsx
@@ -1,3 +1,10 @@
 export default function Overnight() {
-  return <div>Overnight</div>;
+  <div className="pt-[30px]">
+    <h1 className="text-[black]">Գիշերակացի առկայություն</h1>
+    <div className="grid grid-cols-2 gap-2 pt-[10px] w-[200px]">
+      <button>Բոլորը</button>
+      <button>Այո</button>
+      <button>Ոչ</button>
+    </div>
+  </div>;
 }
