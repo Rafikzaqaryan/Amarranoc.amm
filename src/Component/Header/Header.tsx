71e9a8969946f0a3b4c97971daa115a6722ed62e
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:27:25 +0400

    Third Search Part

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -1,11 +1,13 @@
 import Department from "./Department";
 import Logo from "./Logo";
+import Search from "./Search";
 
 export default function Header() {
   return (
     <div className="flex justify-around gap-2 items-center  py-[24px] [@media(max-width:524px)]:gap-[10px]">
       <Logo />
       <Department />
+      <Search />
     </div>
   );
 }
