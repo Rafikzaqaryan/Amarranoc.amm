46b1d495a1656acdd8cc32ab1b9f39e35f723406
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:55:17 +0400

    Done Rooms Part

--- src/Component/Nav/NavLeftPart/Rooms.tsx
+++ src/Component/Nav/NavLeftPart/Rooms.tsx
@@ -1,3 +1,14 @@
 export default function Rooms() {
-  return <div>Rooms</div>;
+  return (
+    <div className="pt-[30px]">
+      <h1 className="text-[black]">Սենյակների քանակ</h1>
+      <div className="flex flex-wrap gap-x-3 gap-y-1 pt-[10px] ">
+        <button>Բոլորը</button>
+        {[1, 2, 3, 4, 5].map((roomCount) => (
+          <button key={roomCount}>{roomCount}</button>
+        ))}
+        <button>6 և ավելի</button>
+      </div>
+    </div>
+  );
 }
