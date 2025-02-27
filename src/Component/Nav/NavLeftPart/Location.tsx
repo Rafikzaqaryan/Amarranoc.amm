ea63d1f94bbc9508cebce485e73cae0d5381fee2
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:45:19 +0400

    Done Location apperance

--- src/Component/Nav/NavLeftPart/Location.tsx
+++ src/Component/Nav/NavLeftPart/Location.tsx
@@ -1,3 +1,12 @@
 export default function Location() {
-  return <div>Location</div>;
+  return (
+    <div>
+      <div className="p-2">
+        <h2 className="text-[black]">Տարածարջան</h2>
+      </div>
+      <div className="h-40 pl-[10px] overflow-y-scroll scrollbar scrollbar-w-12 scrollbar-bg-red-500 flex flex-col gap-y-2">
+        {/* {sortedCategories} */}
+      </div>
+    </div>
+  );
 }
