663685efbc2b824528d5df4394bb9ccf154d58fd
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:18:37 +0400

    Done Logo apperance

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,3 +1,7 @@
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className=" h-[44px]  [@media(min-width:1390px)]:w-auto">
+      <img src="https://amaranoc.am/images/logo.svg" className="w-[160px]" />
+    </div>
+  );
 }
