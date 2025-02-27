e9668cd1fb55490918ac0f3cf36f74f05b415e18
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-28 00:46:20 +0400

    Second MinMaxFIlter Part

---
+++ src/Component/Nav/NavLeftPart/MinMaxFilter.tsx
@@ -0,0 +1,3 @@
+export default function MinMaxFilter() {
+  return <div>MinMaxFilter</div>;
+}
--- src/Component/Nav/NavLeftPart/NavLeftPart.tsx
+++ src/Component/Nav/NavLeftPart/NavLeftPart.tsx
@@ -1,9 +1,11 @@
 import Location from "./Location";
+import MinMaxFilter from "./MinMaxFilter";
 
 export default function NavLeftPart() {
   return (
     <div>
       <Location />
+      <MinMaxFilter />
     </div>
   );
 }
